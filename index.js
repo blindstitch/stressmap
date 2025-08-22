9eb4c5e55b58bcec3f9efb149070b19daea07974
Karl Tacheron <karltacheron@gmail.com>
2025-08-22 11:23:13 -0400

    first

---
+++ index.js
@@ -0,0 +1,258 @@
+const baseWidth = 4;
+const baseZoom = 12
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidHNjaGllZ2dtIiwiYSI6ImNrZHoyb25iYjMxMWQzM2p6eHlibHJkanIifQ.wj-SisFvNPgakxBy_1ZnHw';
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    center: [-72.667638, 42.3164662], // Noho starting position [lng, lat]
+    style:'mapbox://styles/mapbox/dark-v11',
+    // style: 'mapbox://styles/mapbox/standard',
+    // config: {
+    //     basemap: {
+    //     lightPreset: 'day',
+    //     showPlaceLabels: false,
+    //     showPointOfInterestLabels: false,
+    //     theme: 'monochrome',
+    //     show3dObjects: false,
+    //     showTransitLabels: true,
+    //     showRoadLabels: true
+    //     }
+    // },
+    zoom: baseZoom // starting zoom
+});
+
+map.on('load', function () {
+    map.addSource('LTS_source', {
+        type: 'geojson',
+        data: 'plots/LTS.json'
+    });
+    const colors = [
+        'black',
+        // Colors based on 5 equally spaced from 'turbo' colormap
+        '#28BCEB',
+        '#A4FC3C',
+        '#FB7E21',
+        '#7A0403',
+        'white'
+    ]
+    const no_data = 'no data'
+    const LTS_names = [
+        no_data, // black
+        'Level of Traffic Stress 1',
+        'Level of Traffic Stress 2',
+        'Level of Traffic Stress 3',
+        'Level of Traffic Stress 4',
+        no_data // white
+    ]
+
+    map.addLayer({
+        'id': 'lts-layer',
+        'source': 'LTS_source',
+        //'source-layer': 'lts', // replaces 'road-label-simple' which seems to work for light-v11 but not standard style
+        // 'slot': 'middle',
+        "type": "line",
+        'paint': {
+            'line-color': [
+                'match',
+                ['get', 'LTS'],
+                0, colors[0],
+                // Colors based on 5 equally spaced from 'turbo' colormap
+                1, colors[1],
+                2, colors[2],
+                3, colors[3],
+                4, colors[4],
+                colors[5]
+            ],
+            "line-width": baseWidth,
+            // 'line-opacity': 0.5,
+        }
+    },
+    'road-label-simple' // Add layer below labels
+);
+map.setFilter('lts-layer', ['<=', ['get', 'zoom'], map.getZoom()]);
+
+// create legend
+const legend = document.getElementById('legend');
+
+LTS_names.forEach((LTS_name, i) => {
+    // Do not add black or white to the legend since they do not correspond to a LTS rating
+    if (LTS_name === no_data) {
+        return
+    }
+  const color = colors[i];
+  const item = document.createElement('div');
+  const key = document.createElement('span');
+  key.className = 'legend-key';
+  key.style.backgroundColor = color;
+
+  const value = document.createElement('span');
+  value.innerHTML = `${LTS_name}`;
+  item.appendChild(key);
+  item.appendChild(value);
+  legend.appendChild(item);
+});
+
+    // When a click event occurs on a feature in the places layer, open a popup at the
+    // location of the feature, with description HTML from its properties.
+    map.on('click', 'lts-layer', (e) => {
+        // Copy coordinates array.
+        const coordinates = e.features[0].geometry.coordinates.slice(); // I don't think this works with line strings
+        const description = `
+        <h1>${e.features[0].properties.name}</h1>
+        <p>Road Type: ${e.features[0].properties.highway}<br></p>
+        <table>
+            <tr>
+                <th>Value</th>
+                <th>Left/Rev</th>
+                <th>Right/Fwd</th>
+            </tr>
+            <tr>
+                <td><b>LTS</b></td>
+                <td><b>${e.features[0].properties.LTS_rev}</b></td>
+                <td><b>${e.features[0].properties.LTS_fwd}</b></td>
+            </tr>
+            <tr>
+                <td colspan="3"><b>Bike Infrastructure</b></td>
+            </tr>
+            <tr>
+                <td>Bike Permitted</td>
+                <td>${e.features[0].properties.bike_allowed_rev}</td>
+                <td>${e.features[0].properties.bike_allowed_fwd}</td>
+            </tr>
+            <tr>
+                <td>Bike Lane</td>
+                <td>${e.features[0].properties.bike_lane_rev}</td>
+                <td>${e.features[0].properties.bike_lane_fwd}</td>
+            </tr>
+            <tr>
+                <td>Separation</td>
+                <td>${e.features[0].properties.separation_rev}</td>
+                <td>${e.features[0].properties.separation_fwd}</td>
+            </tr>
+            <tr>
+                <td>Width</td>
+                <td>${e.features[0].properties.bike_width_rev} <font color="gray">${e.features[0].properties.bike_width_rule_rev}</font></td>
+                <td>${e.features[0].properties.bike_width_fwd} <font color="gray">${e.features[0].properties.bike_width_rule_fwd}</font></td>
+            </tr>
+            <tr>
+                <td>Buffer</td>
+                <td>${e.features[0].properties.buffer_rev} <font color="gray">${e.features[0].properties.bike_width_rule_rev}</font></td>
+                <td>${e.features[0].properties.buffer_fwd} <font color="gray">${e.features[0].properties.bike_width_rule_fwd}</font></td>
+            </tr>
+            <tr>
+                <td>Reach</td>
+                <td>${e.features[0].properties.bike_reach_rev}</td>
+                <td>${e.features[0].properties.bike_reach_fwd}</td>
+            </tr>
+
+            <tr>
+                <td colspan="3"><b>Street Design for Cars</b></td>
+            </tr>
+            <tr>
+                <td>Oneway</td>
+                <td colspan="2">${e.features[0].properties.oneway} <font color="gray">${e.features[0].properties.street_narrow_wide}</font></td>
+            </tr>
+            <tr>
+                <td>Street Width</td>
+                <td colspan="2">${e.features[0].properties.width_street} <font color="gray">${e.features[0].properties.width_street_rule}</font></td>
+            </tr>
+            <tr>
+                <td>Parking</td>
+                <td>${e.features[0].properties.parking_rev}</td>
+                <td>${e.features[0].properties.parking_fwd}</td>
+            </tr>
+            <tr>
+                <td>Parking Width</td>
+                <td>${e.features[0].properties.parking_width_rev}</td>
+                <td>${e.features[0].properties.parking_width_fwd}</td>
+            </tr>
+            <tr>
+                <td>Lane Count</td>
+                <td colspan="2">${e.features[0].properties.lane_count}</td>
+            </tr>
+            <tr>
+                <td>Prevailing Speed</td>
+                <td colspan="2">${e.features[0].properties.speed} <font color="gray">${e.features[0].properties.speed_rule}</font></td>
+            </tr>
+            <tr>
+                <td>Centerline</td>
+                <td colspan="2">${e.features[0].properties.centerline} <font color="gray">${e.features[0].properties.centerline_rule}</font></td>
+            </tr>
+            <tr>
+                <td>Average Daily Traffic (ADT)</td>
+                <td colspan="2">${e.features[0].properties.ADT} <font color="gray">${e.features[0].properties.ADT_rule}</font></td>
+            </tr>
+            <tr>
+                <td colspan="3"><b>Technical Info</b></td>
+            </tr>
+            <tr>
+                <td>OSMid</td>
+                <td colspan="2"><a href="https://www.openstreetmap.org/way/${e.features[0].properties.osmid}" target="_blank">${e.features[0].properties.osmid}</a></td>
+            </tr>
+            <tr>
+                <td>Zoom</td>
+                <td colspan="2">${e.features[0].properties.zoom}</td>
+            </tr>
+            <tr>
+                <td>LTS_biking_permitted</td>
+                <td>${e.features[0].properties.LTS_biking_permitted_rev}</td>
+                <td>${e.features[0].properties.LTS_biking_permitted_fwd}</td>
+            </tr>
+            <tr>
+                <td>LTS_mixed</td>
+                <td>${e.features[0].properties.LTS_mixed_rev}</td>
+                <td>${e.features[0].properties.LTS_mixed_fwd}</td>
+            </tr>
+            <tr>
+                <td>LTS_bikelane_noparking</td>
+                <td>${e.features[0].properties.LTS_bikelane_noparking_rev}</td>
+                <td>${e.features[0].properties.LTS_bikelane_noparking_fwd}</td>
+            </tr>
+            <tr>
+                <td>LTS_bikelane_yesparking</td>
+                <td>${e.features[0].properties.LTS_bikelane_yesparking_rev}</td>
+                <td>${e.features[0].properties.LTS_bikelane_yesparking_fwd}</td>
+            </tr>
+            <tr>
+                <td>LTS_separation</td>
+                <td>${e.features[0].properties.LTS_separation_rev}</td>
+                <td>${e.features[0].properties.LTS_separation_fwd}</td>
+            </tr>
+            <tr>
+                <td>Parse</td>
+                <td colspan="2">${e.features[0].properties.parse}</td>
+            </tr>
+        </table>`;
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        if (['mercator', 'equirectangular'].includes(map.getProjection().name)) {
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+                coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+        }
+
+        new mapboxgl.Popup()
+            .setLngLat(e.lngLat) // Changed to use click location instead of feature location (I think)
+            .setHTML(description)
+            .setMaxWidth("600px")
+            .addTo(map);
+    });
+
+    // Change the cursor to a pointer when the mouse is over the LTS layer.
+    map.on('mouseenter', 'lts-layer', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'lts-layer', () => {
+        map.getCanvas().style.cursor = '';
+    });
+
+    map.on('zoom', () => {
+        document.getElementById('zoom').textContent = map.getZoom().toFixed(2);
+        map.setFilter('lts-layer', ['<=', ['get', 'zoom'], map.getZoom()]);
+    });
+})
+
